d15b27a1b84d8c69b9424a4f235319c1e5b30604
tunderix <sami.repo.koti@gmail.com>
2015-07-19 09:06:08 +0300

    Initial Commit. ProjectFiles.

---
+++ Assets/Animations/UI/NotificationControl.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotificationControl : MonoBehaviour {
+
+
+	void destroyThis()
+	{
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Prefabs/Objects/PathMarker/DestroyParentObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyParentObject : MonoBehaviour {
+
+	void DestroyPathMarkerParent()
+	{
+		Destroy (this.gameObject.transform.parent.gameObject);
+	}
+}
